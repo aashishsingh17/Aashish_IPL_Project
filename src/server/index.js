974605fc35ae728fab7d3fbbb6f7fbd4bab39c4e
Aashish Kumar Singh <aashish.singh.17@mountblue.tech>
2021-08-23 05:14:38 +0530

    solved 2

--- src/server/index.js
+++ src/server/index.js
@@ -21,3 +21,30 @@ export function matchesPlayedPerYear() {
             fs.writeFileSync('src/public/output/matchesPerYear.json', JSON.stringify(matchesPerYear), 'utf-8');
         })
 }
+
+
+export function matchesWonPerTeamPerYear() {
+    csv()
+        .fromFile(csvFilePath)
+        .then((jsonObj) => {
+            var data1 = JSON.stringify(jsonObj, ['season', 'winner']);
+            var data2 = JSON.parse(data1);
+            //console.log(data2);
+
+            let obj = data2.reduce(function (output, current) {
+                if (output.hasOwnProperty(current.winner)) {
+                    if (output[current.winner].hasOwnProperty(current.season))
+                        output[current.winner][current.season] = output[current.winner][current.season] + 1;
+                    else
+                        output[current.winner][current.season] = 1;
+                } else {
+                    output[current.winner] = {};
+                    output[current.winner][current.season] = 1;
+                }
+                return output;
+            }, {});
+            //console.log(obj);  
+            fs.writeFileSync('src/public/output/matchesWonPerYear.json', JSON.stringify(obj), 'utf-8');
+        })
+}
+
